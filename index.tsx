ded5b562ed19b59df423ecf9c3490da7edad05fa
EzequielCaste <ezequiel.caste@gmail.com>
2021-07-02 18:50:59 -0300

    add footer and mobile responsive styles

--- index.tsx
+++ index.tsx
@@ -4,6 +4,7 @@ import './style.css';
 import people from './people.json';
 
 import { BirthdayReminder } from './BirthdayReminder';
+import { Footer } from './Footer';
 
 interface AppProps {}
 interface AppState {
@@ -14,9 +15,10 @@ const App = () => {
   const [peopleData, setPeople] = useState(people);
 
   return (
-    <div className="App">
+    <>
       <BirthdayReminder clear={setPeople} people={peopleData} />
-    </div>
+      <Footer />
+    </>
   );
 };
 
